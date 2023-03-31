a3dd326fd9b454bbb483ab3eec60cc33f313af8a
Sarah Fatria <56875681+sarahfatria@users.noreply.github.com>
2023-03-31 14:31:55 +0700

    Add files via upload

---
+++ Customer_segmentation.py
@@ -0,0 +1,60 @@
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+
+df_= pd.read_excel("datasets/online_retail_II.xlsx", sheet_name="Year 2010-2011")
+df = df_.copy()
+df.head()
+
+df.shape
+df.describe().T
+
+df.isnull().sum()
+df.dropna(inplace=True)
+df.shape
+
+df = df[~ df["Invoice"].str.contains("C",na=False)]
+
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+
+df["InvoiceDate"].max()
+today_date = dt.datetime(2011,12,11)
+
+
+rfm = df.groupby("Customer ID").agg({"InvoiceDate": lambda x: (today_date - x.max()).days,
+                                    "Invoice": lambda y: y.nunique(),
+                                    "TotalPrice": lambda z: z.sum()})
+
+rfm.columns = ["recency","frequency","monetary"]
+rfm = rfm[rfm["monetary"]>0]
+
+
+rfm["recency_score"] = pd.qcut(rfm["recency"], 5 , labels=[5,4,3,2,1])
+
+rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"),5, labels=[1,2,3,4,5])
+
+rfm["monetary_score"] = pd.qcut(rfm["monetary"], 5, labels=[1,2,3,4,5])
+
+rfm["RFM_SCORE"] = (rfm["recency_score"].astype(str)+rfm["frequency_score"].astype(str))
+rfm.head()
+
+
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+
+rfm["segment"] = rfm["RFM_SCORE"].replace(seg_map, regex=True)
+
+
+rfm.groupby("segment").agg(["mean", "count"])
